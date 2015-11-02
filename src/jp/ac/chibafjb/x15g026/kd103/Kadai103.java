c74ce8db173d2b99c5d883f9744c99076affed7e
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-02 14:58:02 +0900

    課題103

---
+++ src/jp/ac/chibafjb/x15g026/kd103/Kadai103.java
@@ -0,0 +1,9 @@
+package jp.ac.chibafjb.x15g026.kd103;
+
+public class Kadai103 {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋太郎 2 船橋太郎 3 船橋太郎 4 船橋太郎 5 船橋太郎 6 船橋太郎 7 船橋太郎 8 船橋太郎 9 船橋太郎 10 船橋太郎");
+	}
+
+}
