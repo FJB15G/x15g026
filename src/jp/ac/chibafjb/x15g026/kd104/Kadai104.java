a2e8e15769932c2326021ea857133368e49d32aa
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-02 14:58:28 +0900

    課題104

---
+++ src/jp/ac/chibafjb/x15g026/kd104/Kadai104.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g026.kd104;
+
+public class Kadai104 {
+
+	public static void main(String[] args) {
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+
+	}
+
+}
