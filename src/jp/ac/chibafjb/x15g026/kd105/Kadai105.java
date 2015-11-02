6bb970e7b37de4760c0472b730b4eafbf7fe44ad
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-02 14:59:07 +0900

    課題105

---
+++ src/jp/ac/chibafjb/x15g026/kd105/Kadai105.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd105;
+
+public class Kadai105 {
+
+	public static void main(String[] args) {
+		System.out.println("氏名： 船橋 太郎（ふなばし たろう）");
+		System.out.println("年齢： 28歳");
+		System.out.println("その他： どうでもいいけど独身");
+		
+
+	}
+
+}
