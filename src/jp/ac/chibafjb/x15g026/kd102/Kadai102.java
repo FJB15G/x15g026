ea5b7bb52752d1df4ba3911d4485b14273a9114a
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-02 14:41:38 +0900

    課題102

---
+++ src/jp/ac/chibafjb/x15g026/kd102/Kadai102.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g026.kd102;
+
+public class Kadai102 {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋太郎");
+		System.out.println("2 船橋太郎");
+		System.out.println("3 船橋太郎");
+		System.out.println("4 船橋太郎");
+		System.out.println("5 船橋太郎");
+		System.out.println("6 船橋太郎");
+		System.out.println("7 船橋太郎");
+		System.out.println("8 船橋太郎");
+		System.out.println("9 船橋太郎");
+		System.out.println("10 船橋太郎");
+	}
+
+}
