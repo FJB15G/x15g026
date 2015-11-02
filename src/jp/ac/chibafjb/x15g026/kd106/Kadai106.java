5dc1bff202b38197083181acfae21fe8cd051bdb
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-02 14:59:30 +0900

    課題106

---
+++ src/jp/ac/chibafjb/x15g026/kd106/Kadai106.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g026.kd106;
+
+public class Kadai106 {
+
+	public static void main(String[] args) {
+		System.out.println("  a@@@a a@@@a");
+		System.out.println(" @@@@@@e@@@@@@");
+		System.out.println("(@@@@@@@@@@@@@)");
+		System.out.println(" @@@@@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@");
+		System.out.println("   ~@@@@@@@~");
+		System.out.println("     ~@@@~");
+		System.out.println("      ~@~　　　ハート！");
+
+	}
+
+}
