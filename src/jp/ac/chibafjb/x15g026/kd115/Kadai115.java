1af8b51b93182137df628868c0f65449c7f07431
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 14:47:05 +0900

    課題115

---
+++ src/jp/ac/chibafjb/x15g026/kd115/Kadai115.java
@@ -0,0 +1,29 @@
+package jp.ac.chibafjb.x15g026.kd115;
+
+public class Kadai115 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 2;
+			for(int o = 0;o<b;o++){
+				for(int y = 0;y<a;y++){
+				System.out.print("あいうえお");
+				}
+			System.out.println("");	
+			}
+			for(int o = 0;o<b;o++){
+				for(int y = 0;y<a;y++){
+				System.out.print("かきくけこ");
+				}
+			System.out.println("");	
+			}
+			for(int o = 0;o<b;o++){
+				for(int y = 0;y<a;y++){
+				System.out.print("さしすせそ");
+				}
+			System.out.println("");	
+			}
+		
+	}
+
+}
