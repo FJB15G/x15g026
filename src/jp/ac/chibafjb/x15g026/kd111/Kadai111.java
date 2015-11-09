c58ac3693f11885ac9ab375013da0bafe32d39a1
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 13:59:42 +0900

    課題111

---
+++ src/jp/ac/chibafjb/x15g026/kd111/Kadai111.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd111;
+
+public class Kadai111 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		for(int i = 0;i<a;i++){
+			System.out.println("船橋 太郎");
+		}
+
+	}
+
+}
