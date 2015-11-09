b4c301c5115030b402d10ce5687428f06cabd7ac
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:02:36 +0900

    課題121

---
+++ src/jp/ac/chibafjb/x15g026/kd121/Kadai121.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g026.kd121;
+
+public class Kadai121 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		for(int i = 1;i<=a;i++){
+			System.out.println(i);
+		}
+	}
+
+}
