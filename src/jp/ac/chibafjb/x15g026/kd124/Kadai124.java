078efa63ff2665f8f2f3143151115dc614f02fe2
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:03:28 +0900

    課題124

---
+++ src/jp/ac/chibafjb/x15g026/kd124/Kadai124.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd124;
+
+public class Kadai124 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		for(int i = 0;i<a;i++){
+			System.out.print("☆");
+		}
+
+	}
+
+}
