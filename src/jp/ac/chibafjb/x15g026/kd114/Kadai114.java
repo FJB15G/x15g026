923faae5ee8cab614716915d255a5030a0c0ccd0
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 14:46:27 +0900

    課題114

--- src/jp/ac/chibafjb/x15g026/kd114/Kadai114.java
+++ src/jp/ac/chibafjb/x15g026/kd114/Kadai114.java
@@ -3,7 +3,18 @@ package jp.ac.chibafjb.x15g026.kd114;
 public class Kadai114 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int a = 3;
+		int b = 5;
+		//String s;
+		for(int i = 0;i<b;i++){
+			//s="";
+			for(int y = 0;y<a;y++){
+				System.out.print("＊");
+				//s += "*";
+			}
+			System.out.println("");
+			//System.out.println(s);
+		}
 
 	}
 
