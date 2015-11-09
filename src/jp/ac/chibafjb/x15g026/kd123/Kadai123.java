29905d6753217ec0ab613c66c0bf812e95984ae2
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:03:11 +0900

    課題123

---
+++ src/jp/ac/chibafjb/x15g026/kd123/Kadai123.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd123;
+
+public class Kadai123 {
+
+	public static void main(String[] args) {
+		int a = 11;
+		for(int i = 1;i<a;i++){
+			System.out.print("☆");
+		}
+
+	}
+
+}
