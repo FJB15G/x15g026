337a4758ba44ddd9e0e9fc40aabbd7c4579ae626
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 14:01:03 +0900

    課題113

---
+++ src/jp/ac/chibafjb/x15g026/kd113/Kadai113.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd113;
+
+public class Kadai113 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		for(int i = 0;i<a;i++){
+			System.out.println("＊");
+		}
+
+	}
+
+}
