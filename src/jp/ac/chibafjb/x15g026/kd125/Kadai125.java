117aa7900a7bb3041a31f90a906ecc7935195736
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:03:48 +0900

    課題125

---
+++ src/jp/ac/chibafjb/x15g026/kd125/Kadai125.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd125;
+
+public class Kadai125 {
+
+	public static void main(String[] args) {
+		int a = 30;
+		for(int i = 10;i<=a;i=i+10){
+			System.out.println(i + " ＊");
+		}
+
+	}
+
+}
