26c10fe1a3c1f1f2db7c42971e948cf54038cae4
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:02:55 +0900

    課題122s

---
+++ src/jp/ac/chibafjb/x15g026/kd122/Kadai122.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd122;
+
+public class Kadai122 {
+
+	public static void main(String[] args) {
+		int a = 9;
+		for(int i = 0;i<=a;i++){
+			System.out.println(i);
+		}
+
+	}
+
+}
