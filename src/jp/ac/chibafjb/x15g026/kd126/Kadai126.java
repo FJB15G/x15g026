52f4cebea2027a61da1cf08b871366c2f93ff8dd
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 15:07:19 +0900

    課題126

---
+++ src/jp/ac/chibafjb/x15g026/kd126/Kadai126.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd126;
+
+public class Kadai126 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		for(int i = 0;i<a;i++){
+			System.out.println("12345");
+		}
+
+	}
+
+}
