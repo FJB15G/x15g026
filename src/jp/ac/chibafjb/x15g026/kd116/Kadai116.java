2f904738ef27b74fa8f366e0ad3bcdc4094d09c8
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-09 14:47:25 +0900

    課題116

---
+++ src/jp/ac/chibafjb/x15g026/kd116/Kadai116.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g026.kd116;
+
+public class Kadai116 {
+
+	public static void main(String[] args) {
+		int a = 2;
+		for(int i = 0;i<a;i++){
+			System.out.println("あいうえお あいうえお あいうえお");
+			System.out.println("かきくけこ かきくけこ かきくけこ");
+			System.out.println("さしすせそ さしすせそ さしすせそ");
+		}
+
+	}
+
+}
