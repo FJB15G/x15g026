5e5ebe9ffd1ded1b1cf071c6996566beb7258fef
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:24:06 +0900

    課題137

---
+++ src/jp/ac/chibafjb/x15g026/kd137/Kadai137.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g026.kd137;
+
+public class Kadai137 {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=1;
+		int c=0;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+
+	}
+
+}
