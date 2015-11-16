3e5230e4c2c1b8a14d1bcf800f65f25753f2a243
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 15:04:16 +0900

    課題141

---
+++ src/jp/ac/chibafjb/x15g026/kd141/Kadai141.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g026.kd141;
+
+import java.util.Scanner;
+
+public class Kadai141 {
+
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		System.out.print("整数を入力してください。 --> ");
+		int num =sin.nextInt();
+		sin.close();
+		System.out.println("あなたの入力した数は "+num+" です");
+
+	}
+
+}
