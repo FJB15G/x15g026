e87d01527121e06ab457957c20b0c9a293c7c586
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:23:02 +0900

    課題134

---
+++ src/jp/ac/chibafjb/x15g026/kd134/Kadai134.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g026.kd134;
+
+public class Kadai134 {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		y=x;
+		System.out.println("x = "+x);
+		System.out.println("y = "+y);
+
+	}
+
+}
