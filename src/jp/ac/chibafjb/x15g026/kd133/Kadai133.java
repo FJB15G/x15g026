492c5608e4df503a2c05bf3ea39f761db85a8270
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:22:43 +0900

    課題133

---
+++ src/jp/ac/chibafjb/x15g026/kd133/Kadai133.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g026.kd133;
+
+public class Kadai133 {
+
+	public static void main(String[] args) {
+		int x=5;
+		int y=10;
+		System.out.println("x+y = "+(x+y));
+
+	}
+
+}
