f83f39eef50156eb9b9ad661bbc9b58f3d158121
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:25:47 +0900

    課題138

---
+++ src/jp/ac/chibafjb/x15g026/kdT03/KadaiT03.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kdT03;
+
+public class KadaiT03 {
+
+	public static void main(String[] args) {
+		int a = 70;
+		for(int i=55;i<=a;i+=5){
+			System.out.println(i);
+		}
+
+	}
+
+}
