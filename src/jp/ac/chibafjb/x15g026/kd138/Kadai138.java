b9fd2dbc5dc9c311ab024abe94cc34b48f871806
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:24:29 +0900

    課題138

---
+++ src/jp/ac/chibafjb/x15g026/kd138/Kadai138.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g026.kd138;
+
+public class Kadai138 {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=0;
+		d=a;
+		a=b;
+		b=c;
+		c=d;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+
+	}
+
+}
