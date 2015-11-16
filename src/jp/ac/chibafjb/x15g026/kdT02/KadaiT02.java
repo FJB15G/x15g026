d8426e0adb5be1243651ef01a74a8d1d9a639330
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:25:23 +0900

    課題T02

---
+++ src/jp/ac/chibafjb/x15g026/kdT02/KadaiT02.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kdT02;
+
+public class KadaiT02 {
+
+	public static void main(String[] args) {
+		int a = 102;
+		for(int i=100;i<=a;i++){
+			System.out.println(i);
+		}
+
+	}
+
+}
