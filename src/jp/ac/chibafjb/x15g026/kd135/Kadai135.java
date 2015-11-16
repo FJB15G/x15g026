0f977cea31dc264865a730b5f39ba4a1aaefba68
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:23:25 +0900

    課題135

---
+++ src/jp/ac/chibafjb/x15g026/kd135/Kadai135.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kd135;
+
+public class Kadai135 {
+
+	public static void main(String[] args) {
+		String str1 = "apple";
+		String str2 = "orange";
+		System.out.println("str1 = "+str1);
+		System.out.println("str2 = "+str2);
+
+	}
+
+}
