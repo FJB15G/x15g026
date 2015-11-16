d17c04cced62bb085d7f29455e2081411e87e7e4
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:25:04 +0900

    課題T01

---
+++ src/jp/ac/chibafjb/x15g026/kdT01/KadaiT01.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g026.kdT01;
+
+public class KadaiT01 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 0;
+		for(int i=3;i<=a;i++){
+			b=i-2;
+			System.out.println(b);
+		}
+
+	}
+
+}
