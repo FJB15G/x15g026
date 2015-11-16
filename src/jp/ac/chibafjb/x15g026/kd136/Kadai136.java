0ff07e96437184eae95783c99db79a06442d0350
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-16 14:23:44 +0900

    課題136

---
+++ src/jp/ac/chibafjb/x15g026/kd136/Kadai136.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g026.kd136;
+
+public class Kadai136 {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=1;
+		int c=0;
+		c=b;
+		b=a;
+		a=c;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+
+	}
+
+}
