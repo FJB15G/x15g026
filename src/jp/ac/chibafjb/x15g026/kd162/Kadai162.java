4f9705e17cc2e3b5484a05bf51145591d342858b
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-30 15:01:45 +0900

    課題162

---
+++ src/jp/ac/chibafjb/x15g026/kd162/Kadai162.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g026.kd162;
+
+public class Kadai162 {
+
+	public static void main(String[] args) {
+		int x[] = {33,18,102,47,5};
+		int sum = 0;
+		for(int i = 0;i<x.length;i++){
+			sum = sum + x[i];
+		}
+		System.out.println("合計 = "+sum);
+
+	}
+
+}
