37892ddf29ae823e2ed3d910ee01e008d1106b58
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-30 15:02:15 +0900

    課題T11

---
+++ src/jp/ac/chibafjb/x15g026/kdT11/KadaiT11.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kdT11;
+
+public class KadaiT11 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		for(int i = 0;i<a;i++){
+			System.out.println("Ａ");
+		}
+
+	}
+
+}
