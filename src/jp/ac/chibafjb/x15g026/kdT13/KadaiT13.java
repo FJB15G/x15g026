6fa340b05695d02bed0e01596d47fe163b1b19ce
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-30 15:03:01 +0900

    課題T13

---
+++ src/jp/ac/chibafjb/x15g026/kdT13/KadaiT13.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kdT13;
+
+public class KadaiT13 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		for(int i = 1;i<=a;i+=1){
+			System.out.println("35j0"+i+"1");
+		}
+
+	}
+
+}
