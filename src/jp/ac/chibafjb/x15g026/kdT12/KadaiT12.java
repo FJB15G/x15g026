ee48df13ad430d05c3a1ef524e581e10803cacd3
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-11-30 15:02:34 +0900

    課題T12

---
+++ src/jp/ac/chibafjb/x15g026/kdT12/KadaiT12.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g026.kdT12;
+
+public class KadaiT12 {
+
+	public static void main(String[] args) {
+		int[] x = {30,25,20};
+		for(int i = 0;i<x.length;i++){
+			System.out.print(x[i]+" ");
+		}
+
+	}
+
+}
