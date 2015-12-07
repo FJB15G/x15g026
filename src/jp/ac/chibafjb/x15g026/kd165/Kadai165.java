0ebf34d3fe5c384e371b7f513c78c4849eb74f83
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-12-07 15:15:13 +0900

    課題165

---
+++ src/jp/ac/chibafjb/x15g026/kd165/Kadai165.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g026.kd165;
+
+public class Kadai165 {
+
+	public static void main(String[] args) {
+		int a[] = {5,10,15};
+		int m = 0;
+		int n = 0;
+		for(int i = 2;i>=m;i--){
+			System.out.println("b["+n+"] = "+a[i]);
+			n = n + 1;
+		}
+
+	}
+
+}
