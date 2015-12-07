7271ec16979138f869684e2503f70b73f89ed25f
unknown <x15g026@PC504N021.chiba-fjb.ac.jp>
2015-12-07 15:14:46 +0900

    課題164

--- src/jp/ac/chibafjb/x15g026/kd164/Kadai164.java
+++ src/jp/ac/chibafjb/x15g026/kd164/Kadai164.java
@@ -4,7 +4,7 @@ public class Kadai164 {
 
 	public static void main(String[] args) {
 		int a[] = {5,10,15};
-		int b[];
+		int b[] = {0,0,0};
 		int m = 3;
 		for(int i = 0;i<m;i++){
 			System.out.println("b["+i+"] = "+a[i]);
